bae8f9ef406196ba7ef9cfef39a9fcb2c100a7f4
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 18:38:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,6 +35,8 @@ position_6 = board[win_index_6]
 position_7 = board[win_index_7]
 position_8 = board[win_index_8]
 
+if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  return win_combination[0]
 
  def won(board)
    board.each do |firstvar|
