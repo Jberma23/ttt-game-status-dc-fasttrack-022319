7b38284703fc438e7eb43504c70aea14d45800b3
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 18:16:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,17 +8,17 @@ end
 # top_row_win = [1,2,3]
 # if 
 WIN_COMBINATIONS = [
-  ["top_row_win", 
-    [0,1,2], #top row]
-  ["middle_row_wins",  
-    [3,4,5], #middle row]
-    [6,7,8], #bottom row
   
+  [0,1,2], 
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+
   [0,3,6], #first collumn
   [1,4,5], 
   [2,5,8],
   
   [0,4,8], 
   [2,4,6]
-  ]
+  
+  def win
   
