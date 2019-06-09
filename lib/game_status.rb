50c89682578aa5e236e958c1ab86e13c341879d5
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 18:04:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-board = []
-top_row_win
+# # Define your WIN_COMBINATIONS constant
+# board =  ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+# top_row_win = [1,2,3]
+# if 
