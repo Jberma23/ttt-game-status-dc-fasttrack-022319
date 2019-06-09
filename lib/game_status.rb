d68a78d98d0fe20701fa37754fc03c5268dae9ac
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 18:29:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,14 +19,14 @@ WIN_COMBINATIONS = [
   [0,4,8], 
   [2,4,6] ]
   
- top_row_win = WIN_COMBINATIONS[0]
- middle_row_win = WIN_COMBINATIONS[1]
- bottom_row_win = WIN_COMBINATIONS [2]
- first_column_win = WIN_COMBINATIONS[3]
- second_column_win = WIN_COMBINATIONS[4]
- third_column_win = WIN_COMBINATIONS[5]
- first_diag_win = WIN_COMBINATIONS[6]
- second_diag_win =WIN_COMBINATIONS [7]
+# top_row_win = WIN_COMBINATIONS[0]
+# middle_row_win = WIN_COMBINATIONS[1]
+# bottom_row_win = WIN_COMBINATIONS [2]
+# first_column_win = WIN_COMBINATIONS[3]
+# second_column_win = WIN_COMBINATIONS[4]
+# third_column_win = WIN_COMBINATIONS[5]
+# first_diag_win = WIN_COMBINATIONS[6]
+# second_diag_win =WIN_COMBINATIONS [7]
  def won(board)
    board.each do |firstvar|
      
