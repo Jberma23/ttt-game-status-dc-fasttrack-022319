e2b960e411f47e7aa8235713f2f4737a4e7571b1
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 18:35:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,6 +26,9 @@ WIN_COMBINATIONS = [
 # win_index_7 = WIN_COMBINATIONS[6]
 # win_index_8 =WIN_COMBINATIONS [7]
 position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]
+position_4 = board[win_index_1]
  def won(board)
    board.each do |firstvar|
      puts 
